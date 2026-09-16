392253669e72a15dac95103966c7bfe13b436e5a
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-09-16 20:55:34 -0300

    fix: initialize header system actions

--- desktop/TransPoli/SystemActionsHost.cs
+++ desktop/TransPoli/SystemActionsHost.cs
@@ -4,6 +4,8 @@ namespace TransPoli;
 
 public partial class MainWindow
 {
+    private static readonly bool _systemActionsIntegration = SystemActionsIntegration.Initialize();
+
     internal void TriggerUpdateAction()
         => UpdateButton_Click(this, new RoutedEventArgs());
 
