7a8b8cad5f334c0d243af93da15f5bfa7ddc8a9a
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-09-16 21:43:54 -0300

    Fix V1.0.13 UniformGrid build error in Operations Center

--- desktop/TransPoli/OperationsCenterV13.cs
+++ desktop/TransPoli/OperationsCenterV13.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
