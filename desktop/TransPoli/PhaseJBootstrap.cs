6aa8b04b7b63635a83fd529c4646c9d889434649
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-09-17 00:36:33 -0300

    Fase J: ativar modulo de estatisticas

---
+++ desktop/TransPoli/PhaseJBootstrap.cs
@@ -0,0 +1,7 @@
+namespace TransPoli;
+
+public partial class MainWindow
+{
+    // Instancia o módulo sem registrar atalho de teclado e sem alterar o F10.
+    private readonly TabletPhaseJ _phaseJ = new();
+}
