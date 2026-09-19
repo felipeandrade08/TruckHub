26b77b9f451ab5132f23f9564e6ff60c32de61b5
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-09-18 22:31:37 -0300

    fix: importar UniformGrid nos modulos do TransPoli

--- desktop/TransPoli/Notifications.cs
+++ desktop/TransPoli/Notifications.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
