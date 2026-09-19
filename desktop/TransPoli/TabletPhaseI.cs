095c75cb90bed01524ce18d99d262d7e8ade6d29
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-09-18 22:31:33 -0300

    fix: importar UniformGrid nos modulos do TransPoli

--- desktop/TransPoli/TabletPhaseI.cs
+++ desktop/TransPoli/TabletPhaseI.cs
@@ -10,6 +10,7 @@ using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 using System.Windows.Threading;
 
