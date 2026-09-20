901a082007f477bf2ecd22010ef09d055523e5b0
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-09-19 23:11:48 -0300

    TransPoli: expose local data store to runtime modules

--- desktop/TransPoli/Data/LocalDataStore.cs
+++ desktop/TransPoli/Data/LocalDataStore.cs
@@ -11,8 +11,9 @@ internal sealed class LocalDataStore : IDisposable
         Db = new TransPoliDb();
         new DatabaseInitializer(Db).Initialize();
         LegacyDataMigration.Prepare(this);
+        LocalData.Current = this;
     }
 
     public string DatabasePath => Db.DatabasePath;
-    public void Dispose() => Db.Dispose();
+    public void Dispose() { if (ReferenceEquals(LocalData.Current, this)) LocalData.Current = null; Db.Dispose(); }
 }
