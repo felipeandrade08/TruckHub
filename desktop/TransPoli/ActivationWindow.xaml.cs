ffb698ec760719d57020f859d4e6fab2b0ecfb55
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-09-21 19:20:02 -0300

    fix: corrigir strings quebradas na ActivationWindow

--- desktop/TransPoli/ActivationWindow.xaml.cs
+++ desktop/TransPoli/ActivationWindow.xaml.cs
@@ -22,7 +22,7 @@ public partial class ActivationWindow : Window
     public ActivationWindow()
     {
         InitializeComponent();
-        VersionText.Text = $"v{System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? \"0.0.0\"} • TransPoli";
+        VersionText.Text = $"v{System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "0.0.0"} • TransPoli";
         Loaded += async (_, _) => await RestoreOrRequireActivation();
     }
 
