485fde0bca74b692b1c6b94b28b909ee0b973a73
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-09-23 19:07:17 -0300

    Add TransPoli driver phone behavior

---
+++ desktop/TransPoli/DriverPhoneWindow.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace TransPoli;
+
+public partial class DriverPhoneWindow : Window
+{
+    private readonly DispatcherTimer _clock = new() { Interval = TimeSpan.FromSeconds(1) };
+    public DriverPhoneWindow()
+    {
+        InitializeComponent();
+        _clock.Tick += (_, _) => ClockText.Text = DateTime.Now.ToString("HH:mm");
+        ClockText.Text = DateTime.Now.ToString("HH:mm");
+        _clock.Start();
+        Closed += (_, _) => _clock.Stop();
+    }
+}
