d3a9b9c632091fe9c9cf3b9cd2b0ae735ad763c8
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-09-23 06:31:40 -0300

    Wire expanded HUD customization

--- desktop/TransPoli/HudSettingsWindow.xaml.cs
+++ desktop/TransPoli/HudSettingsWindow.xaml.cs
@@ -22,21 +22,22 @@ public partial class HudSettingsWindow : Window
     {
         EnabledCheck.IsChecked=_settings.Enabled; SpeedCheck.IsChecked=_settings.ShowSpeed; RpmCheck.IsChecked=_settings.ShowRpm; RangeCheck.IsChecked=_settings.ShowRange; OdometerCheck.IsChecked=_settings.ShowOdometer; TripKmCheck.IsChecked=_settings.ShowTripKm;
         RouteCheck.IsChecked=_settings.ShowRoute; CompaniesCheck.IsChecked=_settings.ShowCompanies; ProgressCheck.IsChecked=_settings.ShowProgress; CargoCheck.IsChecked=_settings.ShowCargo;
-        ProfitCheck.IsChecked=_settings.ShowProfit; ExpensesCheck.IsChecked=_settings.ShowExpenses;
-        PositionCombo.SelectedIndex=_settings.Position=="Topo"?0:_settings.Position=="Inferior"?2:1; OpacitySlider.Value=_settings.Opacity; ScaleSlider.Value=_settings.Scale; RefreshLabels();
+        ProfitCheck.IsChecked=_settings.ShowProfit; ExpensesCheck.IsChecked=_settings.ShowExpenses; TripStateCheck.IsChecked=_settings.ShowTripState; EtaCheck.IsChecked=_settings.ShowEta; FuelCheck.IsChecked=_settings.ShowFuel; GearCheck.IsChecked=_settings.ShowGear; ConnectionCheck.IsChecked=_settings.ShowConnection; CompactCheck.IsChecked=_settings.CompactMode;
+        PositionCombo.SelectedIndex=PositionIndex(_settings); OpacitySlider.Value=_settings.Opacity; ScaleSlider.Value=_settings.Scale; XSlider.Value=_settings.CustomX; YSlider.Value=_settings.CustomY; RefreshLabels();
     }
     private void ReadValues()
     {
         _settings.Enabled=EnabledCheck.IsChecked==true; _settings.ShowSpeed=SpeedCheck.IsChecked==true; _settings.ShowRpm=RpmCheck.IsChecked==true; _settings.ShowRange=RangeCheck.IsChecked==true; _settings.ShowOdometer=OdometerCheck.IsChecked==true; _settings.ShowTripKm=TripKmCheck.IsChecked==true;
         _settings.ShowRoute=RouteCheck.IsChecked==true; _settings.ShowCompanies=CompaniesCheck.IsChecked==true; _settings.ShowProgress=ProgressCheck.IsChecked==true; _settings.ShowCargo=CargoCheck.IsChecked==true;
-        _settings.ShowProfit=ProfitCheck.IsChecked==true; _settings.ShowExpenses=ExpensesCheck.IsChecked==true;
-        _settings.Position=(PositionCombo.SelectedItem as System.Windows.Controls.ComboBoxItem)?.Content?.ToString()??"Centro superior"; _settings.Opacity=OpacitySlider.Value; _settings.Scale=ScaleSlider.Value;
+        _settings.ShowProfit=ProfitCheck.IsChecked==true; _settings.ShowExpenses=ExpensesCheck.IsChecked==true; _settings.ShowTripState=TripStateCheck.IsChecked==true; _settings.ShowEta=EtaCheck.IsChecked==true; _settings.ShowFuel=FuelCheck.IsChecked==true; _settings.ShowGear=GearCheck.IsChecked==true; _settings.ShowConnection=ConnectionCheck.IsChecked==true; _settings.CompactMode=CompactCheck.IsChecked==true;
+        _settings.Position=(PositionCombo.SelectedItem as System.Windows.Controls.ComboBoxItem)?.Content?.ToString()??"Centro superior"; _settings.UseCustomPosition=_settings.Position=="Personalizado"; _settings.CustomX=XSlider.Value; _settings.CustomY=YSlider.Value; _settings.Opacity=OpacitySlider.Value; _settings.Scale=ScaleSlider.Value;
     }
     private void SettingChanged(object sender,RoutedEventArgs e){ApplyPreview();}
     private void SettingChanged(object sender,System.Windows.Controls.SelectionChangedEventArgs e){ApplyPreview();}
     private void SettingChanged(object sender,RoutedPropertyChangedEventArgs<double> e){ApplyPreview();}
     private void ApplyPreview(){if(_loading)return; ReadValues(); RefreshLabels(); _onPreview(_settings);}
-    private void RefreshLabels(){OpacityText.Text=$"{_settings.Opacity*100:0}%"; ScaleText.Text=$"{_settings.Scale*100:0}%";}
+    private void RefreshLabels(){OpacityText.Text=$"{_settings.Opacity*100:0}%"; ScaleText.Text=$"{_settings.Scale*100:0}%"; XText.Text=$"{_settings.CustomX*100:0}%"; YText.Text=$"{_settings.CustomY*100:0}%";}
+    private static int PositionIndex(HudSettings s) => s.UseCustomPosition ? 9 : s.Position switch { "Superior esquerdo"=>0, "Topo"=>1, "Superior direito"=>2, "Centro esquerdo"=>3, "Centro"=>4, "Centro direito"=>5, "Inferior esquerdo"=>6, "Inferior"=>7, "Inferior direito"=>8, _=>1 };
     private void Save_Click(object sender,RoutedEventArgs e){ReadValues();_settings.Save();_onPreview(_settings);DialogResult=true;Close();}
     private void Cancel_Click(object sender,RoutedEventArgs e)
     {
@@ -60,7 +61,8 @@ public partial class HudSettingsWindow : Window
         ShowCargo = source.ShowCargo,
         ShowProfit = source.ShowProfit,
         ShowExpenses = source.ShowExpenses,
-        Position = source.Position,
+        ShowTripState = source.ShowTripState, ShowEta = source.ShowEta, ShowFuel = source.ShowFuel, ShowGear = source.ShowGear, ShowAlerts = source.ShowAlerts, ShowConnection = source.ShowConnection, CompactMode = source.CompactMode,
+        Position = source.Position, UseCustomPosition = source.UseCustomPosition, CustomX = source.CustomX, CustomY = source.CustomY,
         Opacity = source.Opacity,
         Scale = source.Scale
     };
@@ -79,7 +81,8 @@ public partial class HudSettingsWindow : Window
         target.ShowCargo = source.ShowCargo;
         target.ShowProfit = source.ShowProfit;
         target.ShowExpenses = source.ShowExpenses;
-        target.Position = source.Position;
+        target.ShowTripState = source.ShowTripState; target.ShowEta = source.ShowEta; target.ShowFuel = source.ShowFuel; target.ShowGear = source.ShowGear; target.ShowAlerts = source.ShowAlerts; target.ShowConnection = source.ShowConnection; target.CompactMode = source.CompactMode;
+        target.Position = source.Position; target.UseCustomPosition = source.UseCustomPosition; target.CustomX = source.CustomX; target.CustomY = source.CustomY;
         target.Opacity = source.Opacity;
         target.Scale = source.Scale;
     }
