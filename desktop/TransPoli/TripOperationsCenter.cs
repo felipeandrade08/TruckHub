e89d24c41b3f64d9ab274480881589249de5eb58
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-09-26 04:17:19 -0300

    Fix WPF UniformGrid namespace

--- desktop/TransPoli/TripOperationsCenter.cs
+++ desktop/TransPoli/TripOperationsCenter.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 
 namespace TransPoli;
