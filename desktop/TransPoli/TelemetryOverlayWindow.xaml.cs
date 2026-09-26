b77c8994bd1d76ecd3a497be406a475751c848af
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-09-26 06:45:31 -0300

    Add LINQ support for HUD event deduplication

--- desktop/TransPoli/TelemetryOverlayWindow.xaml.cs
+++ desktop/TransPoli/TelemetryOverlayWindow.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Interop;
 using System.Windows.Controls;
