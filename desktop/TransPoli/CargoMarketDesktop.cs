0c5a491af1db8fc905c76680636d673980481853
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-09-26 04:39:44 -0300

    Fix cargo market UniformGrid namespace

--- desktop/TransPoli/CargoMarketDesktop.cs
+++ desktop/TransPoli/CargoMarketDesktop.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 using System.Windows.Threading;
 
