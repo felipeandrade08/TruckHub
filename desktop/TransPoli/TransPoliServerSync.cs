f7f9947cc21fec0e0c6f1bb47f0e7841ae67284e
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-09-26 03:06:46 -0300

    Clean sync service UI dependencies

--- desktop/TransPoli/TransPoliServerSync.cs
+++ desktop/TransPoli/TransPoliServerSync.cs
@@ -1,14 +1,11 @@
 using System.IO;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Threading;
 
 namespace TransPoli;
 
